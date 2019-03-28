abe7dd8d42c1f5ef986bf0d02143dca56317ca0a
Kaised <meloiddd@gmail.com>
2019-03-28 23:10:07 +0300

    Кастомизация ответа клиенту с использованием конфигурации

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -5,19 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace WebStore
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -25,9 +30,11 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
+            var response = Configuration["CustomClientResponse"];
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(response);
             });
         }
     }
