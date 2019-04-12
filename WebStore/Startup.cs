fca1407affdaaa01f0b1b9860851e433634254a4
Kaised <meloiddd@gmail.com>
2019-04-13 01:40:28 +0300

    Подключение Связи с браузером

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -43,6 +43,7 @@ namespace WebStore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
             app.UseStaticFiles();
 
