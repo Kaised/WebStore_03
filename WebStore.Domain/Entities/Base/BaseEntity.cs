c308ee50ac171fd51e035cb473141643bb3f386f
Kaised <meloiddd@gmail.com>
2019-04-12 15:54:14 +0300

    Добавление инфраструктуры EF для сущностей проекта

--- WebStore.Domain/Entities/Base/BaseEntity.cs
+++ WebStore.Domain/Entities/Base/BaseEntity.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base.Interfaces;
 
@@ -8,6 +10,7 @@ namespace WebStore.Domain.Entities.Base
     /// <summary>Сущность</summary>
     public abstract class BaseEntity : IBaseEntity
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
@@ -7,8 +8,11 @@ using WebStore.Domain.Entities.Base.Interfaces;
 namespace WebStore.Domain.Entities
 {
     /// <summary>Бренд</summary>
+    [Table("Brands")]
     public class Brand : NamedEntity, IOrderedEntity
     {
         public int Order { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
